82dc52d22a7fb218c0d51870b8656976695b7d69
David Moore <3chordme@gmail.com>
2015-11-09 15:12:17 -0700

    Just saving before trying to add some radio buttons.

--- add/addCtrl.js
+++ add/addCtrl.js
@@ -9,7 +9,14 @@ angular.module('weartherApp')
     console.log("$scope.main is", $scope.main);
     //gets info from <dir-current-weather> because added to scope in dirCurrentWeather.js
 
-
+    function submitWearther() {
+      var newWeartherObject = {
+        temperature: $scope.temperature,
+        main: $scope.main,
+        clothing: $scope.newOutfit,
+        comfortLevel: $scope.comfortLevel, //use radio buttons
+      };
+    }
 
     //upon hitting submit, generate a new object with current weather info
     //and user input, and add that object to the firebase array.
