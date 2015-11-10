2f4ecce3c3ef8297066921e0ceb9364f20f7a80d
David Moore <3chordme@gmail.com>
2015-11-10 09:00:08 -0700

    Small formatting changes.

--- directives/dirCurrentWeather.js
+++ directives/dirCurrentWeather.js
@@ -18,10 +18,12 @@ angular.module('weartherApp')
             //console.log('$scope.currentWeather.tempF is ', $scope.currentWeather.tempF)
             $scope.currentWeather = response;
             $scope.temperature = $scope.currentWeather.tempF; //why isn't this working always????
+            //set a default look into resolve
             console.log('$scope.currentWeather.tempF is' + $scope.currentWeather.tempF);
             //only gets a value assigned AFTER the page changes.
             $scope.main = $scope.currentWeather.main;
           });
+
 
       }
 
