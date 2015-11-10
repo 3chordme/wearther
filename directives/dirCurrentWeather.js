3f412576d9a52c17bb28e8d6d17574d116932aa1
David Moore <3chordme@gmail.com>
2015-11-10 11:03:40 -0700

    Everything working. Twice the code, half the load time. Added weatherService to homeCtrl and addCtrl.

--- directives/dirCurrentWeather.js
+++ directives/dirCurrentWeather.js
@@ -7,25 +7,22 @@ angular.module('weartherApp')
       scope: {
         temperature: '=',
         main: '='
-      },
-      // link: function(scope, elem, attrs) {
-      //
-      // }
-      controller: function($scope, weatherService){
-        weatherService.getWeather("Provo")
-          .then(function(response){
-            console.log('response is running');
-            //console.log('$scope.currentWeather.tempF is ', $scope.currentWeather.tempF)
-            $scope.currentWeather = response;
-            $scope.temperature = $scope.currentWeather.tempF; //why isn't this working always????
-            //set a default look into resolve
-            console.log('$scope.currentWeather.tempF is' + $scope.currentWeather.tempF);
-            //only gets a value assigned AFTER the page changes.
-            $scope.main = $scope.currentWeather.main;
-          });
-
-
       }
+      // controller: function($scope, weatherService){
+      //   weatherService.getWeather("Provo")
+      //     .then(function(response){
+      //       console.log('response is running');
+      //       //console.log('$scope.currentWeather.tempF is ', $scope.currentWeather.tempF)
+      //       $scope.currentWeather = response;
+      //       $scope.temperature = $scope.currentWeather.tempF; //why isn't this working always????
+      //       //set a default look into resolve
+      //       console.log('$scope.currentWeather.tempF is' + $scope.currentWeather.tempF);
+      //       //only gets a value assigned AFTER the page changes.
+      //       $scope.main = $scope.currentWeather.main;
+      //     });
+
+
+      
 
     }
 
