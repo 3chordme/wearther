34b2e8ec03f88a32394d76a964a7d93e724e8758
David Moore <3chordme@gmail.com>
2015-11-12 08:57:57 -0700

    Presentation ready.

--- directives/dirCurrentWeather.js
+++ directives/dirCurrentWeather.js
@@ -8,7 +8,7 @@ angular.module('weartherApp')
         temperature: '=',
         main: '='
       }
-    }
+    };
 
 
   });
--- directives/dirNavBar.js
+++ directives/dirNavBar.js
@@ -4,6 +4,6 @@ angular.module('weartherApp')
     return {
       templateUrl: '/directives/dirNavBarTmpl.html',
       restrict: 'E',
-    }
-    
+    };
+
   });
